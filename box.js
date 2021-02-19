d26f123193fcbbf59b63c85f6033b7de315c6742
Tushit Sharma <77721508+TushitSharma2007@users.noreply.github.com>
2021-02-19 16:18:10 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,13 @@
+class box{
+    constructor(x,y,width,height){
+       
+        this.body=Bodies.rectangle(x,y,width,height);
+        World.add(world, this.body);
+    }
+    display(){
+        var pos=this.body.position;
+        rectMode(CENTER);
+        fill(255);
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+}
